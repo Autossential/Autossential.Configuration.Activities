17d622af0782f4620f79f8e119a6368ade30d4c8
Alexandre T. Perez <alexandretperez@gmail.com>
2022-01-22 16:52:15 -0300

    Extension methods without option arguments to avoid message "An expression tree may not contain a call or invocation that uses optional arguments".

--- source/Autossential.Configuration.Core/ConfigItemExtensions.cs
+++ source/Autossential.Configuration.Core/ConfigItemExtensions.cs
@@ -41,21 +41,20 @@ namespace Autossential.Configuration.Core
             return defaultValue;
         }
 
-        public static string ValueAsString(this ConfigItem item, string defaultValue = default)
+        public static string ValueAsString(this ConfigItem item, string defaultValue)
         {
             if (item?.Value == null) return defaultValue;
             if (item.Value is string value) return value;
             return item.Value.ToString();
         }
-
-        public static int ValueAsInt(this ConfigItem item, int defaultValue = default) => ValueAsType(item, defaultValue);
-        public static double ValueAsDouble(this ConfigItem item, double defaultValue = default) => ValueAsType(item, defaultValue);
-        public static decimal ValueAsDecimal(this ConfigItem item, decimal defaultValue = default) => ValueAsType(item, defaultValue);
-        public static float ValueAsFloat(this ConfigItem item, float defaultValue = default) => ValueAsType(item, defaultValue);
-        public static long ValueAsLong(this ConfigItem item, long defaultValue = default) => ValueAsType(item, defaultValue);
-        public static DateTime ValueAsDateTime(this ConfigItem item, DateTime defaultValue = default) => ValueAsType(item, defaultValue);
-        public static bool ValueAsBoolean(this ConfigItem item, bool defaultValue = default) => ValueAsType(item, defaultValue);
-        public static T[] ValueAsArray<T>(this ConfigItem item, T[] defaultValue = default)
+        public static int ValueAsInt(this ConfigItem item, int defaultValue) => ValueAsType(item, defaultValue);
+        public static double ValueAsDouble(this ConfigItem item, double defaultValue) => ValueAsType(item, defaultValue);
+        public static decimal ValueAsDecimal(this ConfigItem item, decimal defaultValue) => ValueAsType(item, defaultValue);
+        public static float ValueAsFloat(this ConfigItem item, float defaultValue) => ValueAsType(item, defaultValue);
+        public static long ValueAsLong(this ConfigItem item, long defaultValue) => ValueAsType(item, defaultValue);
+        public static DateTime ValueAsDateTime(this ConfigItem item, DateTime defaultValue) => ValueAsType(item, defaultValue);
+        public static bool ValueAsBoolean(this ConfigItem item, bool defaultValue) => ValueAsType(item, defaultValue);
+        public static T[] ValueAsArray<T>(this ConfigItem item, T[] defaultValue)
         {
             if (item?.Value == null) return defaultValue;
             if (item.Value is T[] value) return value;
@@ -69,8 +68,8 @@ namespace Autossential.Configuration.Core
             }
             return FromCollection(item.Value, items => Array.ConvertAll(items.ToArray(), ConvertItem<T>()), defaultValue);
         }
-        public static object[] ValueAsArray(this ConfigItem item, object[] defaultValue = default) => ValueAsArray<object>(item, defaultValue);
-        public static List<T> ValueAsList<T>(this ConfigItem item, List<T> defaultValue = null)
+        public static object[] ValueAsArray(this ConfigItem item, object[] defaultValue) => ValueAsArray<object>(item, defaultValue);
+        public static List<T> ValueAsList<T>(this ConfigItem item, List<T> defaultValue)
         {
             if (item?.Value == null) return defaultValue;
             if (item.Value is List<T> value) return value;
@@ -84,7 +83,21 @@ namespace Autossential.Configuration.Core
             }
             return FromCollection(item.Value, items => items.ToList().ConvertAll(ChangeType<T>), defaultValue);
         }
+        public static List<object> ValueAsList(this ConfigItem item, List<object> defaultValue) => ValueAsList<object>(item, defaultValue);
 
-        public static List<object> ValueAsList(this ConfigItem item, List<object> defaultValue = default) => ValueAsList<object>(item, defaultValue);
+
+        public static string ValueAsString(this ConfigItem item) => ValueAsString(item, default);
+        public static int ValueAsInt(this ConfigItem item) => ValueAsInt(item, default);
+        public static double ValueAsDouble(this ConfigItem item) => ValueAsDouble(item, default);
+        public static decimal ValueAsDecimal(this ConfigItem item) => ValueAsDecimal(item, default);
+        public static float ValueAsFloat(this ConfigItem item) => ValueAsFloat(item, default);
+        public static long ValueAsLong(this ConfigItem item) => ValueAsLong(item, default);
+        public static DateTime ValueAsDateTime(this ConfigItem item) => ValueAsDateTime(item, default);
+        public static bool ValueAsBoolean(this ConfigItem item) => ValueAsBoolean(item, default);
+        public static T[] ValueAsArray<T>(this ConfigItem item) => ValueAsArray<T>(item, default);
+        public static object[] ValueAsArray(this ConfigItem item) => ValueAsArray(item, default);
+        public static List<T> ValueAsList<T>(this ConfigItem item) => ValueAsList<T>(item, default);
+        public static List<object> ValueAsList(this ConfigItem item) => ValueAsList(item, default);
+
     }
 }
