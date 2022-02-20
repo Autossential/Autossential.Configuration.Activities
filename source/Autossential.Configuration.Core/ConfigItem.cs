7a5780770aa396ac3426e6205cc18bf46060af8d
Alexandre T. Perez <alexandretperez@gmail.com>
2022-02-20 15:22:51 -0300

    Code review and improvements.

--- source/Autossential.Configuration.Core/ConfigItem.cs
+++ source/Autossential.Configuration.Core/ConfigItem.cs
@@ -7,28 +7,19 @@ namespace Autossential.Configuration.Core
     [DebuggerDisplay("{Key}: {Value}")]
     public class ConfigItem : IEqualityComparer<ConfigItem>, IEquatable<ConfigItem>
     {
-        public ConfigItem(string key, object value)
+        internal ConfigItem(string key, object value)
         {
             Key = key;
             Value = value;
         }
 
         public string Key { get; }
+
         public object Value { get; set; }
-        internal bool HasKey(string key) => string.Compare(Key, key, true) == 0;
+
+        internal bool HasKey(string key) => Key.Equals(key, StringComparison.OrdinalIgnoreCase);
+
         internal ConfigSection Section { get; set; }
-
-        private string _uniqueKey;
-
-        public string UniqueKey()
-        {
-            return _uniqueKey;
-        }
-
-        internal void SetUniqueKey(string value)
-        {
-            _uniqueKey = value;
-        }
 
         public bool Equals(ConfigItem x, ConfigItem y)
         {
--- source/Autossential.Configuration.Core/ConfigSectionExtensions.cs
+++ source/Autossential.Configuration.Core/ConfigSectionExtensions.cs
@@ -7,18 +7,18 @@ namespace Autossential.Configuration.Core
 
     public static class ConfigSectionExtensions
     {
-        public static string AsString(this ConfigSection section, string keyPath, string defaultValue) => ConfigItemExtensions.ValueAsString(section.AsConfigItem(keyPath), defaultValue);
-        public static int AsInt(this ConfigSection section, string keyPath, int defaultValue) => ConfigItemExtensions.ValueAsInt(section.AsConfigItem(keyPath), defaultValue);
-        public static double AsDouble(this ConfigSection section, string keyPath, double defaultValue) => ConfigItemExtensions.ValueAsDouble(section.AsConfigItem(keyPath), defaultValue);
-        public static decimal AsDecimal(this ConfigSection section, string keyPath, decimal defaultValue) => ConfigItemExtensions.ValueAsDecimal(section.AsConfigItem(keyPath), defaultValue);
-        public static float AsFloat(this ConfigSection section, string keyPath, float defaultValue) => ConfigItemExtensions.ValueAsFloat(section.AsConfigItem(keyPath), defaultValue);
-        public static long AsLong(this ConfigSection section, string keyPath, long defaultValue) => ConfigItemExtensions.ValueAsLong(section.AsConfigItem(keyPath), defaultValue);
-        public static DateTime AsDateTime(this ConfigSection section, string keyPath, DateTime defaultValue) => ConfigItemExtensions.ValueAsDateTime(section.AsConfigItem(keyPath), defaultValue);
-        public static bool AsBoolean(this ConfigSection section, string keyPath, bool defaultValue) => ConfigItemExtensions.ValueAsBoolean(section.AsConfigItem(keyPath), defaultValue);
-        public static T[] AsArray<T>(this ConfigSection section, string keyPath, T[] defaultValue) => ConfigItemExtensions.ValueAsArray(section.AsConfigItem(keyPath), defaultValue);
-        public static object[] AsArray(this ConfigSection section, string keyPath, object[] defaultValue) => ConfigItemExtensions.ValueAsArray(section.AsConfigItem(keyPath), defaultValue);
-        public static List<T> AsList<T>(this ConfigSection section, string keyPath, List<T> defaultValue) => ConfigItemExtensions.ValueAsList(section.AsConfigItem(keyPath), defaultValue);
-        public static List<object> AsList(this ConfigSection section, string keyPath, List<object> defaultValue) => ConfigItemExtensions.ValueAsList(section.AsConfigItem(keyPath), defaultValue);
+        public static string AsString(this ConfigSection section, string keyPath, string defaultValue) => ConfigItemExtensions.ValueAsString(section.GetItem(keyPath), defaultValue);
+        public static int AsInt(this ConfigSection section, string keyPath, int defaultValue) => ConfigItemExtensions.ValueAsInt(section.GetItem(keyPath), defaultValue);
+        public static double AsDouble(this ConfigSection section, string keyPath, double defaultValue) => ConfigItemExtensions.ValueAsDouble(section.GetItem(keyPath), defaultValue);
+        public static decimal AsDecimal(this ConfigSection section, string keyPath, decimal defaultValue) => ConfigItemExtensions.ValueAsDecimal(section.GetItem(keyPath), defaultValue);
+        public static float AsFloat(this ConfigSection section, string keyPath, float defaultValue) => ConfigItemExtensions.ValueAsFloat(section.GetItem(keyPath), defaultValue);
+        public static long AsLong(this ConfigSection section, string keyPath, long defaultValue) => ConfigItemExtensions.ValueAsLong(section.GetItem(keyPath), defaultValue);
+        public static DateTime AsDateTime(this ConfigSection section, string keyPath, DateTime defaultValue) => ConfigItemExtensions.ValueAsDateTime(section.GetItem(keyPath), defaultValue);
+        public static bool AsBoolean(this ConfigSection section, string keyPath, bool defaultValue) => ConfigItemExtensions.ValueAsBoolean(section.GetItem(keyPath), defaultValue);
+        public static T[] AsArray<T>(this ConfigSection section, string keyPath, T[] defaultValue) => ConfigItemExtensions.ValueAsArray(section.GetItem(keyPath), defaultValue);
+        public static object[] AsArray(this ConfigSection section, string keyPath, object[] defaultValue) => ConfigItemExtensions.ValueAsArray(section.GetItem(keyPath), defaultValue);
+        public static List<T> AsList<T>(this ConfigSection section, string keyPath, List<T> defaultValue) => ConfigItemExtensions.ValueAsList(section.GetItem(keyPath), defaultValue);
+        public static List<object> AsList(this ConfigSection section, string keyPath, List<object> defaultValue) => ConfigItemExtensions.ValueAsList(section.GetItem(keyPath), defaultValue);
         public static IEnumerable<ConfigItem> Traverse(this ConfigSection section)
         {
             foreach (var item in section.Items)
@@ -29,6 +29,21 @@ namespace Autossential.Configuration.Core
                     foreach (var subItem in config.Traverse())
                         yield return subItem;
                 }
+            }
+        }
+        public static IEnumerable<KeyValuePair<string, object>> Inspect(this ConfigSection section)
+        {
+            foreach (var item in section.Items)
+            {
+                if (item.Value is ConfigSection config)
+                {
+                    foreach (var subItem in config.Inspect())
+                        yield return subItem;
+                }
+                else
+                {
+                    yield return new KeyValuePair<string, object>((section.UniqueName + ConfigSection.DELIMITER + item.Key).TrimStart(ConfigSection.DELIMITER), item.Value);
+                }                
             }
         }
         public static IEnumerable<ConfigItem> SkipValueSections(this IEnumerable<ConfigItem> items)
